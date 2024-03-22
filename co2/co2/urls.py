be6db0b927e59cd488bee09ce02d5abf0bf4cc7e
Ravshanbek Sharifov <snoopyded@gmail.com>
2024-03-22 17:12:33 +0000

    v0.1 The basis for further development has been created for comparison, all that remains is to add line graphics and a map

--- co2/co2/urls.py
+++ co2/co2/urls.py
@@ -1,22 +1,8 @@
-"""
-URL configuration for co2 project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path
+from co2app.views import main_page, compare_countries, show_on_map
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', main_page, name='main_page'),
+    path('compare/', compare_countries, name='compare_countries'),
+    path('show_on_map/', show_on_map, name='show_on_map'),
 ]
--- co2/co2app/views.py
+++ co2/co2app/views.py
@@ -1,3 +1,43 @@
+# views.py
 from django.shortcuts import render
+from co2app.models import CountryName, CO2Value
 
-# Create your views here.
+
+def main_page(request):
+    return render(request, 'index.html')
+
+
+def compare_countries(request):
+    if request.method == 'POST':
+        country1_code = request.POST.get('country1')
+        country2_code = request.POST.get('country2')
+        start_year = int(request.POST.get('start_year', 1990))  # Default to 1990 if not provided
+        end_year = int(request.POST.get('end_year', 2020))  # Default to 2020 if not provided
+
+        country1 = CountryName.objects.get(country_code=country1_code)
+        country2 = CountryName.objects.get(country_code=country2_code)
+
+        co2_data_country1 = CO2Value.objects.filter(country_code=country1, years__range=(start_year, end_year)).order_by('years').values_list('years', 'value')
+        co2_data_country2 = CO2Value.objects.filter(country_code=country2, years__range=(start_year, end_year)).order_by('years').values_list('years', 'value')
+
+        all_years = list(range(1990, 2021))  # Adjust this range as per your data
+
+        return render(request, 'compare_countries.html', {
+            'countries': CountryName.objects.all(),
+            'country1_name': country1.country_name,
+            'country2_name': country2.country_name,
+            'co2_data_country1': co2_data_country1,
+            'co2_data_country2': co2_data_country2,
+            'all_years': all_years,
+            'start_year': start_year,
+            'end_year': end_year,
+        })
+    else:
+        countries = CountryName.objects.all()
+        all_years = list(range(1990, 2021))  # Adjust this range as per your data
+        return render(request, 'compare_countries.html', {'countries': countries, 'all_years': all_years})
+
+
+def show_on_map(request):
+    # Add functionality to show countries on a map
+    return render(request, 'show_on_map.html')
